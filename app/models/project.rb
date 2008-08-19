d726212feac862ac0bbe81b58b16a4c2ea3fcf51
Eloy Duran <eloy.de.enige@gmail.com>
2008-08-19 21:08:14 +0200

    Started the game!

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,19 @@
 class Project < ActiveRecord::Base
-  has_many :tasks, :dependent => :destroy, :accessible => true
+  use_nested_params = true
   
-  validates_presence_of :name
-  validates_associated :tasks, :on => :update # automatically validated on create
+  if use_nested_params
+    puts 'Using NestedParams'
+    
+    extend NestedParams
+    
+    has_many :tasks, :dependent => :destroy, :nested_params => true
+    
+    validates_presence_of :name
+  else
+    puts 'Using :accessible'
+    has_many :tasks, :dependent => :destroy, :accessible => true
+    
+    validates_presence_of :name
+    #validates_associated :tasks, :on => :update # automatically validated on create
+  end
 end
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,43 @@
-require 'test_helper'
+require File.expand_path('../../test_helper', __FILE__)
 
-class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+describe "Project" do
+  before do
+    @valid_params = { :name => 'NestedParams', :tasks => [
+      { :name => 'Check other implementations' },
+      { :name => 'Try with our plugin' }
+    ]}
+    
+    @project = Project.create(@valid_params)
+    @tasks = @project.tasks
+    
+    @valid_update_params = { :name => 'Dinner', :tasks => {
+      @tasks.first.id.to_s => { :name => "Buy food" },
+      @tasks.last.id.to_s  => { :name => "Cook" }
+    }}
+  end
+  
+  it "should take a hash with tasks hashes and create Task records for them" do
+    lambda do
+      Project.create(@valid_params)
+    end.should.differ('Project.count + Task.count', +3)
+    
+    @project.name.should == 'NestedParams'
+    @project.tasks.map(&:name).sort.should == ['Check other implementations', 'Try with our plugin']
+  end
+  
+  it "should update it's own attributes and the attributes of the child tasks" do
+    @project.attributes = @valid_update_params
+    
+    @project.name.should == 'Dinner'
+    @project.tasks.map(&:name).sort.should == ['Buy food', 'Cook']
+  end
+  
+  it "should automatically save the tasks when the project is saved" do
+    @project.name = 'NestedParams and AutosaveAssociation'
+    @project.tasks.first.name = 'Just start!'
+    @project.save!; @project.reload
+    
+    @project.name.should == 'NestedParams and AutosaveAssociation'
+    @project.tasks.first.name.should == 'Just start!'
   end
 end
