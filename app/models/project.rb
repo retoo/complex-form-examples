9e46c888a556f5689c2f7d7a1cdc5a2ec51dcc77
Eloy Duran <eloy.de.enige@gmail.com>
2008-09-18 23:02:06 +0200

    Comparing against rails :accessible is useless now that we use #tasks_attributes= instead of #tasks=.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,20 +1,8 @@
 class Project < ActiveRecord::Base
-  use_nested_params = true
+  extend NestedParams
   
-  if use_nested_params
-    puts 'Using NestedParams'
-    
-    extend NestedParams
-    
-    # Automatically turns on autosave and thus also validates
-    has_many :tasks, :dependent => :destroy, :nested_params => true, :destroy_missing => true, :reject_empty => true
-    
-    validates_presence_of :name
-  else
-    puts 'Using :accessible'
-    has_many :tasks, :dependent => :destroy, :accessible => true
-    
-    validates_presence_of :name
-    #validates_associated :tasks, :on => :update # automatically validated on create
-  end
+  # Automatically turns on autosave and thus also validates
+  has_many :tasks, :dependent => :destroy, :nested_params => true, :destroy_missing => true, :reject_empty => true
+  
+  validates_presence_of :name
 end
