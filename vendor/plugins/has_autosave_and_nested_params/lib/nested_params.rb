8c376a408584cdbd1c858a7e5f47642ff46b40e6
Eloy Duran <eloy.de.enige@gmail.com>
2008-09-19 00:03:09 +0200

    Cleaned some more.

--- vendor/plugins/has_autosave_and_nested_params/lib/nested_params.rb
+++ vendor/plugins/has_autosave_and_nested_params/lib/nested_params.rb
@@ -130,10 +130,6 @@ module NestedParams
   
   def define_nested_params_for_has_one_association(attr)
     class_eval do
-      define_method("#{attr}_attributes") do
-        send(attr).attributes
-      end
-      
       define_method("#{attr}_attributes=") do |value|
         if value.is_a? Hash
           send("build_#{attr}") if send(attr).nil?
