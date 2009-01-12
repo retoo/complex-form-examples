2b8cf252de872b58ee294aa3547b3d5493436554
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-12 16:35:32 +0100

    Updated vendor rails for latest fix. Updated views to use the current code base.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,13 @@
 module ProjectsHelper
+  def remove_link_unless_new_record(fields, record)
+    unless record.new_record?
+      out = ''
+      out << fields.hidden_field(:_delete)
+      out << link_to_function("remove", "$(this).up('.#{record.class.name.underscore}').hide(); $(this).previous().value = '1'")
+      out
+    end
+  end
+  
   def add_task_link(name, form)
     link_to_function name do |page|
       task = render(:partial => 'task', :locals => { :pf => form, :task => Task.new })
