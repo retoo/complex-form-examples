a234fde4419836f277d7e340657f1d8418911d68
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-27 11:32:45 +0100

    Updated code to use accepts_nested_attributes_for instead of accept_nested_attributes_for.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,8 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :tags
   
   # Automatically turns on autosave and thus also validates
-  accept_nested_attributes_for :author
-  accept_nested_attributes_for :tasks, :tags, :allow_destroy => true, :reject_if => proc { |a| a['name'].blank? }
+  accepts_nested_attributes_for :author
+  accepts_nested_attributes_for :tasks, :tags, :allow_destroy => true, :reject_if => proc { |a| a['name'].blank? }
   
   validates_presence_of :name
 end
